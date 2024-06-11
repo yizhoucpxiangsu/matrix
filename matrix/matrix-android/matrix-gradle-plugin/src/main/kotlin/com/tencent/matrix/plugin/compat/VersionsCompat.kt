f4e139c36eafcff820cfea83b86fadad443f4e14
KangYee <1263817308@qq.com>
2024-06-11 17:18:32 +0800

    feat: add support for AGP 7.2.2

--- matrix/matrix-android/matrix-gradle-plugin/src/main/kotlin/com/tencent/matrix/plugin/compat/VersionsCompat.kt
+++ matrix/matrix-android/matrix-gradle-plugin/src/main/kotlin/com/tencent/matrix/plugin/compat/VersionsCompat.kt
@@ -24,7 +24,8 @@ enum class AGPVersion(
     AGP_3_6_0("3.6.0"),
     AGP_4_0_0("4.0.0"),
     AGP_4_1_0("4.1.0"),
-    AGP_7_0_0("7.0.0")
+    AGP_7_0_0("7.0.0"),
+    AGP_7_2_2("7.2.2"),
 }
 
 class VersionsCompat {
